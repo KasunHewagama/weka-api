5bd0d8eebf5d24637bcf535a2a5bd5ceb2d99768
KasunHewagama <kghewagama1997@gmail.com>
2020-08-06 21:17:45 +0530

    Apriori Algorithm added...!

--- weka-api/src/weka/api/HelloWeka.java
+++ weka-api/src/weka/api/HelloWeka.java
@@ -13,6 +13,20 @@ public class HelloWeka {
 		Instances data = new Instances(new BufferedReader(new FileReader("dataset/supermarket.arff")));
 		Apriori model = new Apriori();
 
+		// Build the model
+
+		// Delta value
+		model.setDelta(0.05);
+
+		// Set No. of Rules
+		model.setNumRules(10);
+
+		// Set Lower Bound Minimum Support
+		model.setLowerBoundMinSupport(0.05);
+
+		// Set Upper bound Minimum Support
+		model.setUpperBoundMinSupport(1.0);
+
 		model.buildAssociations(data);
 		System.out.println(model);
 
