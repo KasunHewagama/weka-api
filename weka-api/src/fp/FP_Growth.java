4a9cf39d8616d6d428f22868983a31416a8ddceb
KasunHewagama <kghewagama1997@gmail.com>
2020-08-07 09:16:23 +0530

    final output...!

---
+++ weka-api/src/fp/FP_Growth.java
@@ -0,0 +1,20 @@
+package fp;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import weka.core.Instances;
+import weka.associations.FPGrowth;
+
+public class FP_Growth {
+
+	public static void main(String[] args) throws Exception {
+		// Load Data
+		Instances data = new Instances(new BufferedReader(new FileReader("dataset/supermarket.arff")));
+		FPGrowth fpgModel = new FPGrowth();
+		fpgModel.setNumRulesToFind(10);
+		fpgModel.buildAssociations(data);
+		System.out.println(fpgModel);
+
+	}
+
+}
